f6380beed2b9fe356c866dc1ed4e96e9eb217736
iskolen <iskolen@mail.ru>
2022-11-21 21:09:45 +0300

    Removed reading characters from the file. Added reading strings and reading characters from a string

--- 1_Task/First_Task/First_Task.cpp
+++ 1_Task/First_Task/First_Task.cpp
@@ -13,6 +13,7 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <cstring>
 
 
 #define operation(c)  ((c) == '+' || (c) == '-'  || (c) == '*' || (c) == '/')
@@ -28,166 +29,167 @@ int main()
 	fileInput.open("input.txt");// открываем файл
 	setlocale(LC_ALL, "Russian");
 
-	char currCh;
-	char prevCh;
-	int convert;
-	int Num1;
-	int Num2;
-	int Num1Max;
-	int Num2Max;
-	int maxInt;
-	char sign;
-	char signMax;
-	string firstNum;
-	string secondNum;
-	int totalNum;
-	bool negative;
-	bool next;
-	string tempNum;
-	string line;
+	char currCh, prevCh, sign, signMax;
+	int convert, num1, num2, numMax1, numMax2, maxInt, i, len, totalNum;
+	bool endFile, negative, nextNum;
+	string firstNum, secondNum, line, lineMax;
 
-	tempNum = "";
-	Num1 = 0;
-	Num2 = 0;
-	maxInt = INT_MIN;
-	prevCh = ' ';
-	currCh = ' ';
-	firstNum = "";
-	secondNum = "";
+	num1 = 0;
+	num2 = 0;
 	totalNum = 0;
-	negative = false;
+	len = 0;
+	maxInt = INT_MIN;
+	currCh = ' ';
+	prevCh = ' ';
+	secondNum = "";
+	firstNum = "";
+	endFile = false;
 
-	while (!fileInput.eof())
+	while (getline(fileInput, line))
 	{
-		currCh = fileInput.get();
-		negative = false;
-		next = false;
-		if (fileInput.eof()) // Удаление последнего символа
+		i = 0;
+		len = line.length();
+		while ((i != len) && (i < len))
 		{
-			break;
-		}
+			currCh = line[i], i++;
+			negative = false;
+			nextNum = false;
 
-		if (currCh == '-') // Определяем, отрицательное ли число
-		{
-			if (!digit(prevCh))
+			if (currCh == '-') // Определяем, отрицательное ли число
 			{
-				negative = true;
-				firstNum += currCh;
-			}
-		}
-		if (digit(currCh)) // Читаем первое число
-		{
-			firstNum += currCh;
-			Num1 = stoi(firstNum);
-		}
-		else // Читаем второе число
-		{
-			if (operation(currCh) && (negative == false))
-			{
-				sign = currCh;
-				currCh = fileInput.get();
-
-				while (digit(currCh) || (next == true))
+				if (!digit(prevCh))
 				{
-
-					if (next == true)
-					{
-						currCh = fileInput.get();
-						next = false;
-					}
-					else
-					{
-						secondNum += currCh;
-						tempNum = secondNum;
-						Num2 = stoi(secondNum);
-						currCh = fileInput.get();
-					}
-
-
-					if ((!digit(currCh)) || (space(currCh))) // Складываем числа и находим максимальное
-					{
-						switch (sign)
-						{
-						case '+':
-							totalNum = Num1 + Num2;
-							if (totalNum > maxInt)
-							{
-								maxInt = totalNum;
-								Num1Max = Num1;
-								Num2Max = Num2;
-								signMax = sign;
-							}
-							firstNum = "";
-							tempNum = secondNum;
-							cout << Num1 << "+" << Num2 << " Сумма двух чисел: " << totalNum << endl;
-							cout << endl;
-							break;
-
-						case '-':
-							totalNum = Num1 - Num2;
-							if (totalNum > maxInt)
-							{
-								maxInt = totalNum;
-								Num1Max = Num1;
-								Num2Max = Num2;
-								signMax = sign;
-							}
-							firstNum = "";
-							tempNum = secondNum;
-							cout << Num1 << "-" << Num2 << " Разница двух чисел: " << totalNum << endl;
-							cout << endl;
-							break;
-
-						case '*':
-							totalNum = Num1 * Num2;
-							if (totalNum > maxInt)
-							{
-								maxInt = totalNum;
-								Num1Max = Num1;
-								Num2Max = Num2;
-								signMax = sign;
-							}
-							firstNum = "";
-							tempNum = secondNum;
-							cout << Num1 << "*" << Num2 << " Произведение двух чисел: " << totalNum << endl;
-							cout << endl;
-							break;
-
-						case '/':
-							totalNum = Num1 / Num2;
-							if (totalNum > maxInt)
-							{
-								maxInt = totalNum;
-								Num1Max = Num1;
-								Num2Max = Num2;
-								signMax = sign;
-							}
-							firstNum = "";
-							tempNum = secondNum;
-							cout << Num1 << "/" << Num2 << " Частное двух чисел: " << totalNum << endl;
-							cout << endl;
-							break;
-						}
-					}
-					if (operation(currCh) || space(currCh))
-					{
-						sign = currCh;
-						firstNum = secondNum;
-						Num1 = stoi(firstNum);
-						secondNum = ' ';
-						next = true;
-					}
+					negative = true;
+					firstNum += currCh;
 				}
 			}
-		}
-		prevCh = currCh;
-		firstNum = ' ';
-		secondNum = ' ';
-	}
-	fileInput.close();
 
+			if (digit(currCh)) // Читаем первое число
+			{
+				while (digit(currCh))
+				{
+					firstNum += currCh;
+					num1 = stoi(firstNum);
+					currCh = line[i], i++;
+					if ((i == len) || (i > len))
+					{
+						break;
+					}
+				}
+				if (operation(currCh) && (negative == false))
+				{
+					sign = currCh;
+					currCh = line[i], i++;
+					while (digit(currCh) || (nextNum == true))
+					{
+						if (nextNum == true)
+						{
+							currCh = line[i], i++;
+							nextNum = false;
+						}
+						else
+						{
+							secondNum += currCh;
+							num2 = stoi(secondNum);
+							currCh = line[i], i++;
+						}
+						if ((!digit(currCh)) || (space(currCh))) // Складываем числа и находим максимальное
+						{
+							switch (sign)
+							{
+							case '+':
+								totalNum = num1 + num2;
+								if (totalNum > maxInt)
+								{
+									maxInt = totalNum;
+									numMax1 = num1;
+									numMax2 = num2;
+									signMax = sign;
+									lineMax = line;
+								}
+								firstNum = "";
+								cout << num1 << "+" << num2 << " Сумма двух чисел: " << totalNum << endl;
+								cout << endl;
+								break;
+
+							case '-':
+								totalNum = num1 - num2;
+								if (totalNum > maxInt)
+								{
+									maxInt = totalNum;
+									numMax1 = num1;
+									numMax2 = num2;
+									signMax = sign;
+									lineMax = line;
+								}
+								firstNum = "";
+								cout << num1 << "-" << num2 << " Разница двух чисел: " << totalNum << endl;
+								cout << endl;
+								break;
+
+							case '*':
+								totalNum = num1 * num2;
+								if (totalNum > maxInt)
+								{
+									maxInt = totalNum;
+									numMax1 = num1;
+									numMax2 = num2;
+									signMax = sign;
+									lineMax = line;
+								}
+								firstNum = "";
+								cout << num1 << "*" << num2 << " Произведение двух чисел: " << totalNum << endl;
+								cout << endl;
+								break;
+
+							case '/':
+								totalNum = num1 / num2;
+								if (totalNum > maxInt)
+								{
+									maxInt = totalNum;
+									numMax1 = num1;
+									numMax2 = num2;
+									signMax = sign;
+									lineMax = line;
+								}
+								firstNum = "";
+								cout << num1 << "/" << num2 << " Частное двух чисел: " << totalNum << endl;
+								cout << endl;
+								break;
+							}
+							if (operation(currCh))
+							{
+								if (sign == '-')
+								{
+									firstNum = secondNum;
+									num1 = stoi(firstNum);
+									num1 = -num1;
+									secondNum = "";
+									nextNum = true;
+									sign = currCh;
+								}
+								else
+								{
+									firstNum = secondNum;
+									num1 = stoi(firstNum);
+									secondNum = "";
+									nextNum = true;
+									sign = currCh;
+								}
+							}
+						}
+					}
+				}
+				firstNum = "";
+			}
+			prevCh = currCh;
+			secondNum = "";
+		}
+	}
+
+	fileInput.close();
 	cout << "Наибольшее число: " << maxInt << endl;
-	cout << "Строка: " << Num1Max << ' ' << signMax << ' ' << Num2Max << endl;
+	cout << "Строка, в которой находится наибольшее число: " << lineMax << endl;
 
 	return 0;
 }
