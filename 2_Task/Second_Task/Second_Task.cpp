6a571f2299fa0cc110678abe36fc75075b244615
iskolen <iskolen@mail.ru>
2022-11-23 11:28:46 +0300

    Started the second task

---
+++ 2_Task/Second_Task/Second_Task.cpp
@@ -0,0 +1,56 @@
+﻿/*
+	Петров Вадим ПС-22
+	Вариант 7
+	Задача:
+		Перед открытием двух железнодорожных касс сформировались
+		2 очереди пассажиров,  причем некоторые из них находятся сразу
+		в  обеих очередях.  Для каждого пассажира известны его места в
+		очередях и необходимое время обслуживания.  Если у какого-либо
+		пассажира очереди подходят одновременно, то он обслуживается в
+		первой   кассе.   Промоделировать   работу    касс,    выдавая
+		последовательно информацию об обслуживании пассажиров (7).
+	Среда разработки: Microsoft Visual Studio 2017
+*/
+
+#include <iostream>
+#include <istream>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <queue>
+
+
+using namespace std;
+
+
+int main()
+{
+	string line, firstIn, secondIn;
+	int len, lineNum;
+	char currCh;
+
+
+	setlocale(LC_ALL, "RUS");
+	string fileName;
+	cout << "Введите имя входного файла: ";
+	cin >> fileName;
+	ifstream fileInput(fileName);// Открываем файл
+	if (!fileInput)
+	{
+		cout << "Файл не найден!" << endl;
+		return 0;
+	}
+	cout << endl;
+
+	while (!fileInput.eof())
+	{
+		currCh = fileInput.get();
+		if (fileInput.eof()) // Удаление последнего символа
+		{
+			break;
+		}
+
+		cout << currCh;
+	}
+}
+
